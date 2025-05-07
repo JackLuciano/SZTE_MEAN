575599f065f5de1b3a4c137991ceb0d24c397f8b
Szabó Szabolcs <jackfunc@gmail.com>
2025-05-07 17:57:27 +0200

    feat: add log and profile routes to the API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,12 +6,16 @@ import protectedRoutes from './routes/protected';
 import categoriesRoute from './routes/api/categories';
 import itemsRoute from './routes/api/items';
 import messagesRoute from './routes/api/messages';
+import logRoute from './routes/api/logs';
+import profileRoute from './routes/api/profile';
 
 const registerRoutes = () => {
     const routes = [
         { path: '/auth', handler: authRoutes },
         { path: '/api', handler: protectedRoutes },
         { path: '/api/messages', handler: messagesRoute },
+        { path: '/api/logs', handler: logRoute },
+        { path: '/api/profile/', handler: profileRoute },
         { path: '/categories', handler: categoriesRoute },
         { path: '/items', handler: itemsRoute },
     ];
