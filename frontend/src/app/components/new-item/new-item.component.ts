135aa6ce7bd0fe7152a310c403b32f5122a021af
Szabó Szabolcs <jackfunc@gmail.com>
2025-05-09 16:41:17 +0200

    feat: Create new item component with HTML, SCSS, and unit tests

---
+++ frontend/src/app/components/new-item/new-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-item',
+  imports: [],
+  templateUrl: './new-item.component.html',
+  styleUrl: './new-item.component.scss'
+})
+export class NewItemComponent {
+
+}
